b50051bf3cf2ff97e60f44f7296e49f5ffdb758e
amuybar <amuyunzubq@gmail.com>
2024-04-10 11:33:50 +0300

    Initial Commit

--- script/todo.js
+++ script/todo.js
@@ -5,13 +5,13 @@
 
 
 
-// Function to add a new todo
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   const task = input.value.trim();
   console.log('Adding todo:', task);
   if (task) {
-      fetch('https://todo-backend-19vf.onrender.com/todos', { // Update the URL for POST request
+      fetch('https://todo-backend-19vf.onrender.com/todos', { 
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
@@ -30,9 +30,9 @@ form.addEventListener('submit', function(event) {
   }
 });
 
-// Function to fetch todos on page load
+
 function fetchTodos() {
-  fetch('https://todo-backend-19vf.onrender.com/todos') // Update the URL for GET request
+  fetch('https://todo-backend-19vf.onrender.com/todos') 
       .then(response => response.json())
       .then(todos => {
           todoList.innerHTML = '';
@@ -47,12 +47,12 @@ function fetchTodos() {
               
               const editIcon = document.createElement('i');
               editIcon.className = 'fas fa-edit edit-icon';
-              editIcon.addEventListener('click', () => handleEdit(todo.id, todo.task)); // Add event listener for edit icon
+              editIcon.addEventListener('click', () => handleEdit(todo.id, todo.task)); 
               li.appendChild(editIcon);
 
               const deleteIcon = document.createElement('i');
               deleteIcon.className = 'fas fa-trash-alt delete-icon';
-              deleteIcon.addEventListener('click', () => handleDelete(todo.id)); // Add event listener for delete icon
+              deleteIcon.addEventListener('click', () => handleDelete(todo.id)); 
               li.appendChild(deleteIcon);
 
               
@@ -61,11 +61,11 @@ function fetchTodos() {
       .catch(error => console.error('Error fetching todos:', error));
 }
 
-// Function to handle edit action
+
 function handleEdit(id, task) {
   const newTask = prompt('Edit task:', task);
   if (newTask !== null && newTask.trim() !== '') {
-      fetch(`https://todo-backend-19vf.onrender.com/todos/${id}`, { // Update the URL for PUT request
+      fetch(`https://todo-backend-19vf.onrender.com/todos/${id}`, { 
           method: 'PUT',
           headers: {
               'Content-Type': 'application/json'
@@ -80,15 +80,15 @@ function handleEdit(id, task) {
   }
 }
 
-// Function to handle delete action
+
 function handleDelete(id) {
   if (confirm('Are you sure you want to delete this task?')) {
-      fetch(`https://todo-backend-19vf.onrender.com/todos/${id}`, { // Update the URL for DELETE request
+      fetch(`https://todo-backend-19vf.onrender.com/todos/${id}`, { 
           method: 'DELETE'
       })
       .then(response => {
           if (response.ok) {
-              fetchTodos(); // Reload todos after delete
+              fetchTodos(); 
           } else {
               console.error('Error deleting todo:', response.statusText);
           }
@@ -98,6 +98,5 @@ function handleDelete(id) {
 }
 
 
-// Fetch todos on page load
-console.log('Fetching todos on page load...');
+
 fetchTodos();
